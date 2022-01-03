93548d3c33f098d643c5f0fcef063e5ecd3e0b17
iamzr <iamziarehman@gmail.com>
2022-01-03 02:18:21 +0000

    Moved siteTitle to navbar.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,59 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
 import styled from "styled-components"
 
-import { rhythm, scale } from "../utils/typography"
+import { rhythm } from "../utils/typography"
 import Navbar from "./navbar"
 
 class Layout extends React.Component {
   render() {
     const { location, title, children } = this.props
-    const rootPath = `${__PATH_PREFIX__}/`
-    const blogPath = `${__PATH_PREFIX__}/blog/`
-    let header
 
-    if (location.pathname === rootPath || location.pathname === blogPath) {
-      header = (
-        <h1
-          style={{
-            ...scale(1.5),
-            marginBottom: rhythm(0.5),
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={location.pathname === blogPath ? `/blog/` : `/`}
-          >
-            {title}
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3
-          style={{
-            fontFamily: `Montserrat, sans-serif`,
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/blog/`}
-          >
-            {title}
-          </Link>
-        </h3>
-      )
-    }
     return (
       <Wrapper>
         <div
@@ -64,8 +18,8 @@ class Layout extends React.Component {
             padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
           }}
         >
-          <header>{header}</header>
-          <Navbar></Navbar>
+          {/* <header>{header}</header> */}
+          <Navbar location={location} title={title}></Navbar>
           <main>{children}</main>
         </div>
         <Footer>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,6 @@
 import { Link } from "gatsby"
 import React from "react"
-import { rhythm } from "../utils/typography"
+import { scale, rhythm } from "../utils/typography"
 
 function Item({ to, name }) {
   return (
@@ -10,22 +10,91 @@ function Item({ to, name }) {
   )
 }
 
-function Navbar() {
-  return (
-    <nav
-      style={{
-        display: `flex`,
-        flex: 1,
-        justifyContent: `center`,
-        marginBottom: rhythm(1),
-      }}
-    >
-      <Item to="/blog/" name={`home`}></Item>
-      <Item to="/tags/fitness" name={`fitness`}></Item>
-      <Item to="/tags/dev" name={`dev.`}></Item>
-      <Item to="/tags/uni" name={`uni`}></Item>
-    </nav>
-  )
+function Navbar({ location, title }) {
+  const rootPath = `${__PATH_PREFIX__}/`
+  const blogPath = `${__PATH_PREFIX__}/blog/`
+  let navbar
+
+  if (location.pathname === rootPath || location.pathname === blogPath) {
+    navbar = (
+      <React.Fragment>
+        <header>
+          <h1
+            style={{
+              ...scale(1.5),
+              marginBottom: rhythm(0.5),
+              marginTop: 0,
+            }}
+          >
+            <Link
+              style={{
+                boxShadow: `none`,
+                textDecoration: `none`,
+                color: `inherit`,
+              }}
+              to={location.pathname === blogPath ? `/blog/` : `/`}
+            >
+              {title}
+            </Link>
+          </h1>
+        </header>
+
+        <nav
+          style={{
+            display: `flex`,
+            flex: 1,
+            justifyContent: `center`,
+            marginBottom: rhythm(1),
+          }}
+        >
+          <Item to="/blog/" name={`home`}></Item>
+          <Item to="/tags/fitness" name={`fitness`}></Item>
+          <Item to="/tags/dev" name={`dev`}></Item>
+          <Item to="/tags/uni" name={`uni`}></Item>
+        </nav>
+      </React.Fragment>
+    )
+  } else {
+    navbar = (
+      <div style={{ display: `flex`, justifyContent: `space-between` }}>
+        <header>
+          <h3
+            style={{
+              fontFamily: `Montserrat, sans-serif`,
+              marginTop: 0,
+            }}
+          >
+            <Link
+              style={{
+                boxShadow: `none`,
+                textDecoration: `none`,
+                color: `inherit`,
+              }}
+              to={`/blog/`}
+            >
+              {title}
+            </Link>
+          </h3>
+        </header>
+
+        <nav
+        //   style={{
+        //     display: `flex`,
+        //     flex: 1,
+        //     justifyContent: `center`,
+        //     marginBottom: rhythm(1),
+        //   }}
+        >
+          <Item to="/blog/" name={`home`}></Item>
+          <Item to="/tags/fitness" name={`fitness`}></Item>
+          <Item to="/tags/dev" name={`dev`}></Item>
+          <Item to="/tags/uni" name={`uni`}></Item>
+        </nav>
+      </div>
+    )
+  }
+
+  return <div>{navbar}</div>
 }
 
 export default Navbar
